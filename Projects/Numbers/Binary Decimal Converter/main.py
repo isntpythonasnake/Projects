f9c1022b70367698d5960651978d13098b6ccdbe
Shark <surlaw1@gmail.com>
2022-10-21 03:30:48 +0800

    Added UI!

--- Projects/Numbers/Binary Decimal Converter/main.py
+++ Projects/Numbers/Binary Decimal Converter/main.py
@@ -1,18 +1,25 @@
-isRunning = True
-bord = str(input("Enter Binary or Decimal to convert to: "))
+import PySimpleGUI as sg
 
-while isRunning:
-    if bord == "Decimal":
-        b = int(input("Decimal to convert to Binary: "))
-        bn = bin(b)
-        print(str(bn).replace("0b", "Answer: "))
-        break
-    elif bord == "Binary":
-        d = input("Binary to convert to Decimal: ")
-        dn = int(d, 2)
-        print("Answer: " + str(dn))
-        break
-    elif bord != "Decimal" or bord != "Binary":
-        print("Please enter either Binary or Decimal!")
-        bord = str(input("Enter Binary or Decimal to convert to: "))
+sg.theme('Reddit')
+layout = [[sg.Text('Convert Binary to Decimal and back!')],
+          [sg.Text('Decimal: '), sg.Input('', key='dtext')],
+          [sg.Text('Binary: '), sg.Input('', key='btext')],
+          [sg.Button('Convert Decimal', key='DtoBButton'), sg.Button('Convert Binary', key='BtoDButton')],
+          [sg.Text('The answer will display here!', key='ans')]
+          ]
 
+window = sg.Window('Converter', layout)
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+    elif event == 'DtoBButton':
+        dvalue = int(values['dtext'])
+        result = bin(dvalue)
+        window["ans"].update(value=str(result).replace("0b", "Answer: "))
+    elif event == 'BtoDButton':
+        bvalue = values['btext']
+        result = int(bvalue, 2)
+        window["ans"].update(value="Answer: " + str(result))
+
+window.close()
