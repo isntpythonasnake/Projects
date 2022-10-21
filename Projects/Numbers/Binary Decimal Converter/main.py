4f2a2dfed8495b503ac6deeab02564cb156d7d99
Shark <surlaw1@gmail.com>
2022-10-21 14:59:18 +0800

    Cleaned up and optimized code

--- Projects/Numbers/Binary Decimal Converter/main.py
+++ Projects/Numbers/Binary Decimal Converter/main.py
@@ -2,10 +2,9 @@ import PySimpleGUI as sg
 
 sg.theme('Reddit')
 layout = [[sg.Text('Convert Binary to Decimal and back!')],
-          [sg.Text('Decimal: '), sg.Input('', key='dtext')],
-          [sg.Text('Binary: '), sg.Input('', key='btext')],
-          [sg.Button('Convert Decimal', key='DtoBButton'), sg.Button('Convert Binary', key='BtoDButton')],
-          [sg.Text('The answer will display here!', key='ans')]
+          [sg.Radio('Binary', key='radio1', group_id=1), sg.Radio('Decimal', key='radio2', group_id=1)],
+          [sg.Text('Input: '), sg.Input('', key='itext')],
+          [sg.Button('CONVERT', key='Button'), sg.Text('The answer will display here!', key='ans')],
           ]
 
 window = sg.Window('Converter', layout)
@@ -13,13 +12,13 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
-    elif event == 'DtoBButton':
-        dvalue = int(values['dtext'])
-        result = bin(dvalue)
-        window["ans"].update(value=str(result).replace("0b", "Answer: "))
-    elif event == 'BtoDButton':
-        bvalue = values['btext']
-        result = int(bvalue, 2)
-        window["ans"].update(value="Answer: " + str(result))
-
+    elif event == 'Button' and values['radio1'] and not values['radio2']:
+        value = int(values['itext'])
+        result = bin(value)
+        window["ans"].update(value=str(result).replace("0b", "Answer in Binary: "))
+    elif event == 'Button' and values['radio2'] and not values['radio1']:
+        value = values['itext']
+        result = int(value, 2)
+        window["ans"].update(value="Answer in Decimal: " + str(result))
+    print(values, event)
 window.close()
